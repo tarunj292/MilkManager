380bad7cf22a9a6c0f44c635719808e2808c052f
tarunj292 <156983323+tarunj292@users.noreply.github.com>
2024-02-03 17:00:22 +0530

    started working on Milk Manager

---
+++ app/src/main/java/com/example/milkmanager/CustomerMilkEntry.kt
@@ -0,0 +1,11 @@
+package com.example.milkmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerMilkEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_milk_entry)
+    }
+}
